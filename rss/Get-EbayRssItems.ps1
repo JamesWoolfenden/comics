61621e078fdf3cdba0d1367da6cfc215926b64ab
James Woolfenden <jim_woolfenden@hotmail.com>
2014-09-28 15:04:00 +0100

    more covers

--- rss/Get-EbayRssItems.ps1
+++ rss/Get-EbayRssItems.ps1
@@ -15,7 +15,15 @@ Function Get-EbayRssItems
 	$xml = Get-RssContent -Keywords $Keywords -ExcludeWords $ExcludeWords -state $state -CategoryId $CategoryId
 	$xml.rss | % {$_.channel.item} | % {
 		$item = $_
-		$items += Parse-ListingInfo $item
+		try
+		{
+		   $items += Parse-ListingInfo $item
+		}
+		catch 
+		{
+           write-warning "`nFailed to parse item"
+		}
 	}
+
 	return $items
 }
