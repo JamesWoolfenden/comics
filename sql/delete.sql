9155b2ac04a95c21bfacda4801b7078279e9c1e0
James Woolfenden <james.woolfenden@gmail.com>
2015-06-13 18:43:23 +0100

    new covers

--- sql/delete.sql
+++ sql/delete.sql
@@ -3,7 +3,7 @@ select * from comics where title='sex criminals' and status = 'expired'
 
 select * from comics where  status = 'open'
 
-delete from comics where title='BIRTHRIGHT' and status = 'expired'
+delete from comics where title='WARRIOR' and status = 'expired'
 select * from comics where Ebayitem='121480430470'
 delete from comics where Ebayitem='121480430470'
 update comics set status='open' where title='THE AUTUMNLANDS' and issue='1A' and status ='verified'
