7283d8457ff883ea5867d442fa2c38d7be7f7e99
James Woolfenden <james.woolfenden@gmail.com>
2015-07-19 11:11:53 +0100

    new prices

--- sql/delete.sql
+++ sql/delete.sql
@@ -1,9 +1,9 @@
 use comics
 select * from comics where title='sabrina' and status = 'expired'
 
-select * from comics where  status = 'open'
+select * from comics where  status = 'closed' and title = 'the walking dead' and issue='1a'
 delete from comics where status = 'open'
 delete from comics where title='chew' and status = 'open'
 select * from comics where Ebayitem='121480430470'
-delete from comics where Ebayitem='351392837907'
+delete from comics.dbo.comics where Ebayitem='291478437944'
 update comics set status='open' where title='THE AUTUMNLANDS' and issue='1A' and status ='verified'
