37814b536e63c950174365e28fff282972824321
James Woolfenden <james.woolfenden@gmail.com>
2015-07-26 18:44:55 +0100

    new covers

--- sql/delete.sql
+++ sql/delete.sql
@@ -1,9 +1,12 @@
 use comics
 select * from comics where title='sabrina' and status = 'expired'
 
-select * from comics where  status = 'open'
-delete from comics where status = 'expired'
-delete from comics where title='the dying and the dead' and status = 'expired'
+select * from comics where  Ebayitem= '141724726278'
+
+delete from [comics].[dbo].comics where title='lazarus' and status = 'expired'
+delete from [comics].[dbo].comics where status = 'open'
+
 select * from comics where Ebayitem='121480430470'
-delete from comics where Ebayitem='121480430470'
+delete from [comics].[dbo].comics where Ebayitem='281691892486'
 update comics set status='open' where title='THE AUTUMNLANDS' and issue='1A' and status ='verified'
+
